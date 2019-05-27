ab0eda2aad3d6cfaf213677aa6bc20318ecca1ce
Eunice Chan <ekchan@berkeley.edu>
2019-05-26 20:41:14 -0700

    Did display part of tutorial

---
+++ main.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+local background = display.newImageRect( "background.png", 360, 570 )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
