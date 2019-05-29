2bd250fe9117a1ba6fa30c2fcad6fd5f4a2a9aed
Eunice Chan <ekchan@berkeley.edu>
2019-05-28 19:14:55 -0700

    Changing to Juggling Game

--- main.lua
+++ main.lua
@@ -10,57 +10,51 @@ local left = 0-(display.actualContentWidth - display.contentWidth)/2
 local top = 0-(display.actualContentHeight - display.contentHeight)/2
 local right = display.contentWidth + (display.actualContentWidth - display.contentWidth)/2
 local bottom = display.contentHeight + (display.actualContentHeight - display.contentHeight)/2
-local ending = "GO UP!"
+
+local endText = "Game over!"
+
+local ballsGroup = display.newGroup()
+local gradient
+
+math.randomseed( os.time() )
 
 -- display
-local background = display.newImageRect( "background.png", 360, 570 )
+local background = display.newImageRect("background.png", 360, 570)
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
-local platform = display.newImageRect( "platform.png", 300, 50 )
-platform.x = display.contentCenterX
-platform.y = display.contentHeight-25
+local ball = display.newCircle(112, 112, 50)
+gradient = {
+    type="gradient",
+    color1={ 1, 0.2, 0.1 }, color2={ 0.7, 0.8, 0.3 }, direction="down"
+}
+ball:setFillColor(gradient)
 
-local balloon = display.newImageRect( "balloon.png", 112, 112 )
-balloon.x = display.contentCenterX
-balloon.y = display.contentCenterY
-
-local endText = display.newText( ending, display.contentCenterX, 20, native.systemFont, 40 )
+local endText = display.newText("", display.contentCenterX, 20, native.systemFont, 40 )
+endText:setFillColor(0, 0, 0)
 
 
 -- physics
-local physics = require( "physics" )
+local physics = require("physics")
 physics.start()
-physics.addBody( platform, "static" )
-physics.addBody( balloon, { radius=50, bounce=0.3 } )
+physics.setGravity(0, 9)
+physics.addBody(ball, {radius=50})
 
 
 -- interaction
-local function pushBalloon()
-    balloon:applyLinearImpulse( 0, -0.75, balloon.x, balloon.y )
+local function pushBall()
+    ball:applyTorque(10)
+    ball:applyLinearImpulse( 0, -0.75, ball.x, ball.y )
 end
 
-balloon:addEventListener( "tap", pushBalloon )
+ball:addEventListener( "tap", pushBall )
 
-local function isOnScreenByCenter( self, event )
+local function isAtBottom( self, event )
    buffer = self.y/2
-   if( (self.x + self.x/2) > right ) then
-        ending = "game over"
-        endText.text = "game over"
-   end
-   if( (self.x - self.x/2) < left) then
-        ending = "game over"
-        endText.text = "game over"
-   end
    if( (self.y + self.y/2) > bottom ) then
-        ending = "game over"
-        endText.text = "game over"
-   end
-   if( (self.y - self.y/2) < top ) then
-        ending = "game over"
-        endText.text = "game over"
+        endText.text = endText
    end
 end
 
-balloon.enterFrame = isOnScreenByCenter
-Runtime:addEventListener( "enterFrame", balloon )
+ball.enterFrame = isAtBottom
+Runtime:addEventListener( "enterFrame", ball )
